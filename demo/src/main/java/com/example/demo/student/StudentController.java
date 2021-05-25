c451a21ce629d96e2044b55c1ed63784ffd8669e
Walter Ruganzu <rwalter692@gmail.com>
2021-05-25 16:41:48 +0300

    Finalizing the build up

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -2,14 +2,12 @@ package com.example.demo.student;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/student")
+
 public class StudentController {
 
     private final StudentService studentService;
@@ -20,8 +18,24 @@ public class StudentController {
     }
 
 
-    @GetMapping
+    @GetMapping("/student")
     public List getStudents(){
         return studentService.getStudents();
+    }
+
+    @PostMapping("/student")
+    public String addNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+        return "New student created";
+    }
+
+    @DeleteMapping("/student/{id}")
+    public String deleteStudent(@PathVariable long id){
+        return studentService.deleteStudent(id);
+    }
+
+    @PutMapping("/student/{id}")
+    public void updateStudent(@PathVariable long id, @RequestParam(required = false) String name, @RequestParam(required = false) String email){
+        studentService.updateStudent(id, name, email);
     }
 }
